f64a13e75fd69a8774ec18ab5f97c8f5510c9476
jthebat <taein007@gmail.com>
2023-04-18 13:29:20 +0900

    #4

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -58,7 +58,7 @@ server.on('request',(req,res) => {
 
 });
 */
-/**#3
+/** #3
  * const server = http.createServer();
 
 const friends = [
@@ -106,7 +106,8 @@ server.on('request',(req,res) => {
     }     
 });
  */
-const server = http.createServer();
+/** #4
+ * const server = http.createServer();
 
 const friends = [
     {
@@ -133,6 +134,61 @@ server.on('request',(req,res) => {
             friends.push(JSON.parse(friend));
         })
         
+    } else if( req.method === 'GET' && items[1]==='friends') {        
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json') 
+        if(items.length ===3 ) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+        
+    } else if ( req.method === 'GET' && items[1] ==='messages') {
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello Issac!</li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('</ul>');    
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {    
+        res.statusCode = 404;
+        res.end()
+    }     
+});
+ */
+const server = http.createServer();
+
+const friends = [
+    {
+        id:0,
+        name: 'Sir Issac Newton'
+    },
+    {
+        id:1,
+        name: 'Albert Einstein'
+    },
+    {
+        id:2,
+        name: 'Nicola Tesla'
+    }
+]
+
+server.on('request',(req,res) => {  
+    const items = req.url.split('/');
+    // /friends/2  => ['', 'friends', '2']
+    // /friends/
+    if ( req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:' , data);            
+            friends.push(JSON.parse(friend));
+        })
+        req.pipe(res)
     } else if( req.method === 'GET' && items[1]==='friends') {        
         res.statusCode = 200;
         res.setHeader('Content-Type','application/json') 
