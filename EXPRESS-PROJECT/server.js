1161917467bce0198e7d3cb58bd8fee33f589e32
jthebat <taein007@gmail.com>
2023-04-25 23:54:14 +0900

    res header test

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -4,6 +4,21 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT,() => {
+//Express is really good at routing.
+
+app.get('/', (req, res) => {
+    res.send({
+        id:1,
+        name:"Issac Newton"
+    });
+});
+app.get('/messages', (req, res) => {
+    res.send('<ul><li>Hello Albert!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+    console.log('Updating messages...');
+});
+app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`)
 })
